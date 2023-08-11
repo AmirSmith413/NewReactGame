c893122b55e2f18fc146dfdf8ad7ae8e946a9b6b
AmirSmith413 <amirsmith166@gmail.com>
2023-08-10 19:05:03 -0700

    started adding loading skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,6 +13,7 @@ export interface Game {
   name: string;
   background_image: string
   parent_platforms:{platform: Platform}[]
+  metacritic: number
 }
 
 interface FetchGameResponse {
@@ -23,19 +24,28 @@ interface FetchGameResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
   useEffect(() => {
 
    const controller = new AbortController()
 
+   setIsLoading(true)
+
     apiClient
       .get<FetchGameResponse>("/games",{signal: controller.signal})
-      .then((response) => setGames(response.data.results))
+      .then((response) => {
+        setIsLoading(false)
+        setGames(response.data.results)
+      })
       .catch((error) => {
-        if(error instanceof CanceledError)
-        setError(error.message)});
+        if(error instanceof CanceledError) return
+        setError(error.message)
+        setIsLoading(false)
+      });
+      
       return () => controller.abort()
   }, []);
-  return {games,error}
+  return {games,error,isLoading}
 };
 
 export default useGames;
